969409a6dc4dadf0d6a6d755e6514f181755d3aa
lcottret <ludovic.cottret@inrae.fr>
2021-03-09 14:09:54 +0100

    remove vuetify from app

--- src/App.vue
+++ src/App.vue
@@ -9,41 +9,20 @@
 // See the License for the specific language governing permissions and
 //    limitations under the License.
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <!-- <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        /> -->
-        <div class="text-h3">AliTreeViz</div>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <span class="mr-2 caption"
-        >Explore multiple sequence alignments and phylogenetic trees
-        together</span
-      >
-    </v-app-bar>
-
-    <v-main>
-      <alitreeviz />
-    </v-main>
-  </v-app>
+<div>
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <h1 class="title">AliTreeViz</h1>
+    </div>
+    <div class="navbar-end">
+      <span class="caption">Explore multiple sequence alignments and phylogenetic trees
+        together</span>
+    </div>
+  </nav>
+  <section class="section">
+    <alitreeviz />
+  </section>
+</div>
 </template>
 
 <script>
@@ -61,3 +40,23 @@ export default {
   })
 }
 </script>
+
+<style scoped>
+h1{
+  color:white;
+  font-size: 32px;
+}
+
+nav {
+  padding: 10px;
+  height: 50px;
+}
+
+.main {
+  top: 150px;
+}
+
+.caption {
+  font-size:12px;
+}
+</style>
