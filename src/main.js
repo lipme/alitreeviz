61b4fb041722d81e59b60e6bb7149211e778140b
lcottret <ludovic.cottret@inrae.fr>
2021-03-09 16:01:22 +0100

    remove vue-router

--- src/main.js
+++ src/main.js
@@ -11,13 +11,11 @@
 
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 
 require('@/assets/main.scss')
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
